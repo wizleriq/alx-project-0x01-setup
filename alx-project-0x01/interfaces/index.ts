7231be1e7938362539c33bd8a5dcf215ab058f5f
Ndata <wisdomndata@gmail.com>
2025-05-30 15:44:59 +0100

    More Components (Extending Post Page)

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -56,3 +56,15 @@ export interface UserProps {
     bs: string;
   };
 }
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body:string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
