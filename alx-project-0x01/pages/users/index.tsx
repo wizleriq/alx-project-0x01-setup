96bd61cc2a65996ce058bac6513b80513bd1d7fb
Ndata <wisdomndata@gmail.com>
2025-05-30 14:32:00 +0100

    Implement Users Card Components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -28,7 +28,7 @@ const Users: React.FC<UserProps[]> = ({posts}) => {
   )
 }
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
   const posts = await response.json()
 
